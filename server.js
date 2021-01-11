2e3a06546a7772036b66cb08730282694d720482
Anthony Pena <bursosoy@gmail.com>
2021-01-11 19:22:55 +0800

    Added Heroku dependencies
    Added Heroku dependencies

---
+++ server.js
@@ -0,0 +1,14 @@
+// All required for Heroku deployment
+
+const express = require('express');
+const servStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
+const enforce = require('express-sslify');
+
+const app = express();
+
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(serveStatic(_dirname + '/dist'));
+app.use(history());
+app.listen(process.env.PORT ||  5000);
